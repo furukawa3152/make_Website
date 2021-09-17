03842b95ed2def4e843c4b8e0c58d68176691fb3
itaruchan <itaru_furukawa@ocomoji.co.jp>
2021-09-17 23:22:34 +0900

    サーバー引っ越したので遷移画面のみに

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+setTimeout(function(){
+  window.location.href = 'https://itaruweb.com/';
+}, 5*1000);
+
 const btn =document.querySelector('.btn-menu')
 const nav =document.querySelector('.top-nav')
 btn.addEventListener('click',() =>{
